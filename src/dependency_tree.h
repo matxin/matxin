b09554852d107a0e37874f29a079936d81d15620
matthew <m5w@users.noreply.github.com>
2017-01-11 15:45:42 -0500

    Initial version of the test (segfault)

--- src/dependency_tree.h
+++ src/dependency_tree.h
@@ -1,6 +1,8 @@
 #ifndef DEPENDENCY_TREE_H
 #define DEPENDENCY_TREE_H
 
+#include "dependency_tree_node.h"
+
 // std::wistream
 #include <istream>
 
@@ -21,7 +23,11 @@ protected:
   friend DependencyTreeNode;
   std::map<long, DependencyTreeNode> nodes_;
 
-  decltype(nodes_) get_nodes() { return nodes_; }
+  const decltype(nodes_) &get_nodes() const { return nodes_; }
+  decltype(nodes_) &get_nodes() {
+    return const_cast<decltype(nodes_) &>(
+        static_cast<const decltype(this)>(this)->get_nodes());
+  }
 
 private:
   void get_not_empty_line(std::wstring &line, std::wistream &conll_u);
--- src/test/deserialise_conll_u.cc
+++ src/test/deserialise_conll_u.cc
@@ -1,4 +1,30 @@
 #include "../dependency_tree.h"
 
-int main() {
+// EXIT_FAILURE
+#include <cstddef>
+
+// std::exception
+#include <exception>
+
+// std::wcin
+#include <iostream>
+
+// std::getline
+#include <string>
+
+int main(int argc, char **argv) {
+  try {
+    std::wstring line;
+    std::getline(std::wcin, line);
+
+    if (!std::wcin) {
+      std::cerr << argv[0] << ": Unexpected end-of-file\n";
+      return EXIT_FAILURE;
+    }
+
+    matxin::DependencyTree dependency_tree(std::move(line), std::wcin);
+  } catch (const std::exception &exception_) {
+    std::cerr << argv[0] << ": " << exception_.what() << "\n";
+    return EXIT_FAILURE;
+  }
 }
